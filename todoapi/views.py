b1ce5dbb4a9d88d25f0e9512e3556fe8f313b017
Jireh Angelo S. Quirog <quirogybo21@gmail.com>
2025-04-13 21:20:50 +0800

    updated 04.13.2025

--- todoapi/views.py
+++ todoapi/views.py
@@ -1,7 +1,20 @@
 from rest_framework import viewsets
+from rest_framework.permissions import IsAuthenticated  
 from .models import Task
 from .serializers import TaskSerializer
+from django.http import JsonResponse
 
 class TaskViewSet(viewsets.ModelViewSet):
+    permission_classes = [IsAuthenticated]  
     queryset = Task.objects.all().order_by('-created_at')
     serializer_class = TaskSerializer
+
+
+def home(request):
+    return JsonResponse({
+        "message": "🎉 Welcome to the Todo API!",
+        "routes": {
+            "Tasks endpoint": "/api/tasks/",
+            "Token auth": "/api-token-auth/"
+        }
+    })
--- todobackend/urls.py
+++ todobackend/urls.py
@@ -2,11 +2,15 @@ from django.contrib import admin
 from django.urls import path, include
 from rest_framework import routers
 from todoapi import views
+from rest_framework.authtoken.views import obtain_auth_token 
+from todoapi import views
 
 router = routers.DefaultRouter()
 router.register(r'tasks', views.TaskViewSet)
 
 urlpatterns = [
+    path('', views.home),
     path('admin/', admin.site.urls),
     path('api/', include(router.urls)),
+    path('api-token-auth/', obtain_auth_token, name='api_token_auth'),  
 ]
